606818723a4f48c519e0827d831e37eef678d007
jasysak <47327644+jasysak@users.noreply.github.com>
2021-02-05 22:16:52 -0500

    Update FizzBuzz.py

--- python/FizzBuzz.py
+++ python/FizzBuzz.py
@@ -1,14 +1,15 @@
 # The 'seminal' FizzBuzz problem
 # from -50 to 100...in python. 
 # Almost too easy...it works!
-
+printed = 0
 i = -50
 while i <=100: 
     if (i % 3 == 0) and (i % 5 ==0): 
         print("FizzBuzz")
-    if (i % 3 == 0):
+        printed = 1
+    if (i % 3 == 0) and (printed <> 1):
         print("Fizz")
-    if (i % 5 == 0):
+    if (i % 5 == 0) and (printed <> 1):
         print("Buzz")
     else:
         print (i)
